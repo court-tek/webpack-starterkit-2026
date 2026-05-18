68c47a1238ac54335ef288ada5a81953a2b7435d
court-tek <dishoughng@gmail.com>
2026-05-19 00:41:11 +0200

    first commit

---
+++ assets/js/hyper/actions/index.js
@@ -0,0 +1,8 @@
+function test(name) {
+    return function(data) {
+        return `${name} ${data}`
+    }
+}
+
+// export default aSentence = test('courtney');
+
---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+export default {
+    mode: "development",
+    entry: "./assets/js/hyper/app.js",
+    output: {
+        path: path.resolve(__dirname, "./public/dist"),
+        filename: "bundle.js"
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                use: "babel-loader",
+                exclude: /node_modules/
+            },
+            {
+                use: ["style-loader", "css-loader", "sass-loader"],
+                test:/\.scss$/,
+            }
+        ]
+    },
+  devServer: {
+    static: {
+      directory: path.join(__dirname, "public"),
+    },
+    compress: true,
+    port: 9000,
+  },
+};
